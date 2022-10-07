da2419dd32b23931989d8f5db7e82a2deebd3c95
YoungBear <youngbear@aliyun.com>
2022-10-07 16:20:30 +0800

    [update] add EventListener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -28,6 +30,11 @@ public class DemoApplication {
         return route(GET("/hello-world"),
                 request -> ok().body(Mono.just("Hello, World"), String.class)
         );
+    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("当前 WebServer 实现类为： " + event.getWebServer().getClass().getName());
     }
 
     public static void main(String[] args) {
